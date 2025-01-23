c2641406f8286741039b8db2fa82344153af5e5b
Facundo Duarte <facundoduarte1313@gmail.com>
2025-01-23 17:04:05 -0300

    feat: enable theme support in index.js by importing view from @forge/bridge

--- static/frontend/src/index.js
+++ static/frontend/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.tsx';
-
+import { view} from "@forge/bridge"
 import '@atlaskit/css-reset';
+
+await view.theme.enable();
 
 ReactDOM.render(
     <React.StrictMode>
